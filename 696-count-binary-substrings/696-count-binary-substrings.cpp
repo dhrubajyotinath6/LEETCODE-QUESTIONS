a1332c8ad9b3d33b32d2a002bbd97b610714e3ed
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-17 14:02:56 +0530

    Time: 23 ms (84.07%), Space: 9.4 MB (99.78%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -1,6 +1,6 @@
 /*
 https://leetcode.com/problems/count-binary-substrings/discuss/1172569/Short-and-Easy-w-Explanation-and-Comments-or-Keeping-Consecutive-0s-and-1s-Count-or-Beats-100
-*/
+
 class Solution {
 public:
     int countBinarySubstrings(string &s) {
@@ -22,4 +22,32 @@ public:
         ans += min(prev, cur);
         return ans;
     }
+};
+*/
+class Solution {
+public:
+    
+int countBinarySubstrings(string& s) {
+    
+  // we start from first character - so curConsecutive = 1
+  int curConsecutive = 1, prevConsecutive = 0, ans = 0;
+    
+  for(int i = 1; i < size(s); i++) {
+      
+  	if(s[i] != s[i - 1]){ // whenever current & previous don't match
+        
+  		ans += min(prevConsecutive, curConsecutive), // number of substring formed is minimum of cur & prev count
+  		prevConsecutive = curConsecutive,            // previous consecutive streak will become current consecutive and,
+  		curConsecutive = 1;                          // current streak will be resetted
+    }
+  	else {
+        curConsecutive++; 
+    }
+  		
+  }
+  // required to include count from last two groups of consecutive characters.
+  ans += min(prevConsecutive, curConsecutive); 
+    
+  return ans;
+}
 };
