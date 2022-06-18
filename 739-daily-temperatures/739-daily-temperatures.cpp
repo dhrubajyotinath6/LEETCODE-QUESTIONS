d55e8badfcf9e78e666bd48b10b80847ce7b4506
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-18 12:51:13 +0530

    Time: 246 ms (43.32%), Space: 85 MB (95.64%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,3 +1,6 @@
+//https://leetcode.com/problems/daily-temperatures/solution/
+
+/*
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
@@ -18,3 +21,51 @@ public:
         return ans;
     }
 };
+*/
+
+
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        int n = temperatures.size();
+        int hottest = 0;
+        vector<int> ans(n,0);
+        
+        for(int currDay = n - 1; currDay >= 0; currDay--){
+            
+            if(temperatures[currDay] >= hottest){
+                hottest = temperatures[currDay];
+                continue;
+            }
+            
+            int days = 1;
+            
+            while(temperatures[currDay + days] <= temperatures[currDay]){
+                //days++;  --> TLE
+                days += ans[currDay + days];
+            }
+            
+            ans[currDay] = days;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
