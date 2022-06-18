452aaefc30257a5b62793e9800be4f59dd9d4a54
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-18 19:28:58 +0530

    Time: 28 ms (81.69%), Space: 11 MB (77.67%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/flip-string-to-monotone-increasing/discuss/189751/C%2B%2B-one-pass-DP-solution-0ms-O(n)-or-O(1)-one-line-with-explaination.
+
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
