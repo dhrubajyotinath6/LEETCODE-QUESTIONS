f1c2ff89f7d7ad1825cb58ac1845f3bdc15076b6
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-19 11:15:54 +0530

    Time: 5 ms (83.81%), Space: 5.9 MB (77.91%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -5,6 +5,7 @@ https://leetcode.com/problems/integer-to-roman/solution/
 https://leetcode.com/problems/integer-to-roman/discuss/1293176/C%2B%2B-easy-solution-O(1)
 */
 
+/*
 class Solution {
 public:
     string intToRoman(int num) 
@@ -23,5 +24,18 @@ public:
         }
         
         return res;
+    }
+};
+*/
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
