e5af0e1ecea7e55502a3c9e38504eaf6adf13654
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-22 08:59:11 +0530

    Time: 12 ms (5.82%), Space: 9.1 MB (23.89%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.cpp
@@ -11,6 +11,7 @@
  https://leetcode.com/problems/find-leaves-of-binary-tree/solution/
  */
 
+/*
 class Solution {
 public:
     
@@ -64,7 +65,69 @@ public:
         return solution;
     }
 };
+*/
+
+/*
+Complexity Analysis
+
+Time Complexity: Assuming NN is the total number of nodes in the binary tree, traversing the tree takes O(N)O(N) time. Sorting all the pairs based on their height takes O(N \log N)O(NlogN) time. Hence overall time complexity of this approach is O(N \log N)O(NlogN)
+
+Space Complexity: O(N)O(N), the space used by pairs. solution also requires O(N)O(N) space however the output does not count towards the space complexity.
+
+====================================================================
+*/
 
 
+class Solution {
+private:
+
+    vector<vector<int>> solution;
+
+public:
+    
+    int getHeight(TreeNode *root) {
+        
+        // return -1 for null nodes
+        if (!root) {
+            return -1;
+        }
+
+        // first calculate the height of the left and right children
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+        
+        // based on the height of the left and right children, obtain the height of the current (parent) node
+        int currHeight = max(leftHeight, rightHeight) + 1;
+        
+        // create space for node located at `currHeight` if not already exists
+        if (this->solution.size() == currHeight) {
+            this->solution.push_back({});
+        }
+
+        // insert the value at the correct position in the solution array
+        this->solution[currHeight].push_back(root->val);
+        
+        // return the height of the current node
+        return currHeight;
+    }
+    
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        
+        this->solution.clear();
+        
+        getHeight(root);
+        
+        return this->solution;
+    }
+};
+
+
+/*
+Complexity Analysis
+
+Time Complexity: Assuming NN is the total number of nodes in the binary tree, traversing the tree takes O(N)O(N) time and storing all the pairs at the correct position also takes O(N)O(N) time. Hence overall time complexity of this approach is O(N)O(N).
+
+Space Complexity: O(N)O(N), the space used by solution array.
+*/
 
 
