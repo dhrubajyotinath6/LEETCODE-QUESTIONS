bd6a03f019b50d08bbf6087de1fbd7bed630e10e
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-24 10:27:10 +0530

    Time: 5 ms (49.91%), Space: 7.8 MB (60.73%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,3 +1,7 @@
+/* 
+Runtime: 4 ms, faster than 62.49% of C++ online submissions for Relative Sort Array.
+Memory Usage: 7.6 MB, less than 93.45% of C++ online submissions for Relative Sort Array.
+
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
@@ -22,6 +26,28 @@ public:
                 arr1[i++] = n;
             }
         }
+        return arr1;
+    }
+};
+*/
+//what if the constraints are not there?
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        
+        map<int, int> m;        
+        for (auto i : arr1) m[i]++;
+        
+        int pos = 0;
+        for (auto j : arr2) {
+            while(m[j]-- > 0) arr1[pos++] = j;
+        }
+        
+        
+        for (auto it : m) {
+            while(it.second-- > 0) arr1[pos++] = it.first;
+        }
+        
         return arr1;
     }
 };
