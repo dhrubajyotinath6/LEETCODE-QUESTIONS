520881a62b0c619810400f427b8270e7e129cbc9
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-29 13:30:10 +0530

    Time: 15 ms (40.94%), Space: 9.7 MB (59.75%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -28,6 +28,7 @@ public:
 };
 */
 
+/*
 class Solution {
     
 public:
@@ -57,4 +58,34 @@ public:
         
         return dp[n][color] = totalCost;
     }
+};
+
+*/
+
+
+class Solution {
+    
+public:
+    int minCost(vector<vector<int>>& costs) {
+        
+        if (costs.size() == 0) return 0; 
+        
+        for (int n = costs.size() - 2; n >= 0; n--) {
+            
+            // Total cost of painting the nth house red.
+            costs[n][0] += min(costs[n + 1][1], costs[n + 1][2]);
+            
+            // Total cost of painting the nth house green.
+            costs[n][1] += min(costs[n + 1][0], costs[n + 1][2]);
+            
+            // Total cost of painting the nth house blue.
+            costs[n][2] += min(costs[n + 1][0], costs[n + 1][1]);
+        }
+
+         
+
+        return min(min(costs[0][0], costs[0][1]), costs[0][2]);
+    }
+    
+  
 };
