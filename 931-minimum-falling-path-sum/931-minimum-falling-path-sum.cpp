5a039b4b0aec4fa4f231b9bf29aa27f37ccdb33c
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-06-29 15:22:28 +0530

    Time: 23 ms (44.25%), Space: 10.2 MB (61.03%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -76,6 +76,7 @@ public:
 
 */
 
+/*
 class Solution {
     
 public:
@@ -102,6 +103,43 @@ public:
         
         for (int startCol = 0; startCol < matrix.size(); startCol++) {
             minFallingSum = min(minFallingSum, dp[0][startCol]);
+        }
+        return minFallingSum;
+    }
+};
+
+*/
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        vector<int> dp(matrix.size() + 1, 0);
+        
+            for (int row = matrix.size() - 1; row >= 0; row--) {
+
+                vector<int> currentRow(matrix.size() + 1, 0);
+
+                for (int col = 0; col < matrix.size(); col++) {
+
+                    if (col == 0) {
+                        currentRow[col] = min(dp[col], dp[col + 1]) + matrix[row][col];
+                    } 
+                    else if (col == matrix.size() - 1) {
+                        currentRow[col] = min(dp[col], dp[col - 1]) + matrix[row][col];
+                    } else {
+                        currentRow[col] = min(dp[col], min(dp[col + 1], dp[col - 1])) + matrix[row][col];
+                    }
+                }
+
+                dp = currentRow;
+            }
+        
+        
+        int minFallingSum = INT_MAX;
+        
+        for (int startCol = 0; startCol < matrix.size(); startCol++) {
+            minFallingSum = min(minFallingSum, dp[startCol]);
         }
         return minFallingSum;
     }
