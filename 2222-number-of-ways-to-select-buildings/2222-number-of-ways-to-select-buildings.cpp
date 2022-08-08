93c70d5843be36304ff9f46fdbc4ee235954e361
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-08-08 11:59:54 +0530

    Time: 103 ms (94.09%), Space: 24.4 MB (58.74%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,5 +1,9 @@
 //https://leetcode.com/problems/number-of-ways-to-select-buildings/discuss/1907026/Easy-understanding-C%2B%2B-code-with-comments
 
+/*
+https://leetcode.com/problems/number-of-ways-to-select-buildings/discuss/1907179/JavaPython-3-One-pass-T-O(n)-S-O(1)-codes-and-follow-up-w-brief-explanation-and-analysis.    --READ THIS TOO 
+*/
+
 //DP ALSO POSSIBLE CHECK THE DISCUSSION SECTION
 /*
 The possible sequences of selected buildings is either "101" or "010".
@@ -10,6 +14,16 @@ product of the number of occurances of building '1' before the current building
 to the number of occurances of building '1' after the current building 
 and viceversa.
 
+*/
+
+/*
+So, lets traverse the string and consider the current character as the centeral character of the combination.
+
+    This means, if the current character is '0', then all we need to do it 
+find the number of 1 before this 0 and number of 1 after this 0 
+and multiply them to add them to the answer.
+    We do the same for the central character as '1' and
+count the number of 0 before and after this one.
 */
 
 
