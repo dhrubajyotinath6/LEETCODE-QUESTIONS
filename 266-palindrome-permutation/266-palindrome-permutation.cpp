8e4b508e483054ffcee2b1a7cbf46979927554e7
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-08-22 17:48:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.36%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.cpp
+++ 266-palindrome-permutation/266-palindrome-permutation.cpp
@@ -1,3 +1,15 @@
+/*
+TC -> O(N) {but we r iterating 2 times}
+SC -> O(1)
+
+every number twice
+    or
+every number twice and one number odd times
+
+So, we are finding whether we hv more than one odds or not
+
+*/
+/*
 class Solution {
 public:
     bool canPermutePalindrome(string s) {
@@ -17,5 +29,42 @@ public:
         }
         
         return true;
+    }
+};
+*/
+
+
+
+/*
+We can solve in one loop also
+
+https://leetcode.com/problems/palindrome-permutation/solution/ 
+REFER APPROACH 4
+*/
+
+class Solution {
+public:
+    bool canPermutePalindrome(string s) {
+        
+        vector<int> mpp(26,0);
+        int count = 0;
+        
+        for(int i = 0; i < s.size(); i++){
+            
+            int val = (s[i] - 'a');
+            
+            mpp[val]++;
+            
+            if(mpp[val] % 2 == 0){
+                count--;
+            }
+            else
+                count++;
+        }
+        
+       //if all characters are occuring even times, count = 0
+       // if one character occuring odd time, count = 1;
+       // if more than one character occuring odd times, count > 1
+       return count <= 1; 
     }
 };
