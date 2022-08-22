58e3981361b0f76bc3ddfb289b18d49223b3fe4f
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-08-22 13:28:21 +0530

    Time: 2 ms (83.50%), Space: 6.7 MB (15.12%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int longestPalindrome(string s) {
@@ -26,9 +27,44 @@ public:
             
         }
         
-        cout << single << endl;
+        // cout << single << endl;
         
         return (ans + single);
+        
+    }
+};
+*/
+
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        
+    vector<int> mpp(128,0);
+        
+    for(auto ch : s){
+        mpp[ch - '0']++;
+    }
+        
+    int ans = 0;
+    bool flag = false;
+      
+    for(auto i : mpp){
+        
+        //even
+        if( i%2 == 0){
+            ans += i;
+        }
+        
+        else if(i % 2 == 1){
+            ans += i - 1;
+            flag = true;
+        }
+    }
+        
+        if(flag) return ans + 1;
+        else return ans;
+        
         
     }
 };
