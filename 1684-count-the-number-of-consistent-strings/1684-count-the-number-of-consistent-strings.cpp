2c2272ea25986f4929d4cb25e08c3be88b28c68f
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-09-08 23:06:12 +0530

    Time: 80 ms (69.64%), Space: 30.3 MB (29.08%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,33 +1,29 @@
-//https://leetcode.com/problems/count-the-number-of-consistent-strings/discuss/969668/C%2B%2B-easy-solution    --FRIENDLY
-
-// https://leetcode.com/problems/count-the-number-of-consistent-strings/discuss/969522/C%2B%2B-all_of --votrubac
-
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
         
-        int mpp[26] = {0}, ans = 0;
+        set<char> hashSet;
         
-        for(auto &ch : allowed){
-            mpp[ch - 'a']++;
+        for(auto ch : allowed){
+            hashSet.insert(ch);
         }
         
-        for(auto &word : words){
+        int ans = 0;
+        
+        for(auto word : words){
             
             bool isConsistent = true;
             
-            for(auto &ch : word){
+            for(auto ch : word){
                 
-                if(mpp[ch - 'a'] == 0){
-                    
+                if(!hashSet.count(ch)){
                     isConsistent = false;
-                    break;
-                }                
+                }
             }
+            
             if(isConsistent) ans++;
         }
         
         return ans;
-        
     }
 };
