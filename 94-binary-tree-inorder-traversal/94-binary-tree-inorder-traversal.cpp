b1ddfea0e2d8920fa6ae8ec68d2de8182ca28c0a
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-09-08 08:37:08 +0530

    Time: 4 ms (41.62%), Space: 8.4 MB (39.59%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
 class Solution {
     
     void dfs(TreeNode* node, vector<int> &ans){
@@ -33,3 +35,42 @@ public:
         return ans;
     }
 };
+
+*/
+    
+class Solution {    
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+         stack<TreeNode*> st;
+         vector<int> ans;
+
+         TreeNode* node = root;
+
+         if(node == NULL) return ans;
+
+        while(true){
+            
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                
+                if(st.empty() == true) break;
+                
+                node = st.top();
+                st.pop();
+                
+                ans.push_back(node->val);
+                
+                node = node->right;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+    
+    
