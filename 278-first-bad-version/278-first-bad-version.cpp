cdc2b1c4e10631be79b0cbebd9e8072d3bbb9e61
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2022-09-22 08:38:10 +0530

    Time: 3 ms (37.75%), Space: 5.7 MB (99.22%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -12,13 +12,23 @@ public:
         
         while(low <= high){
             
+            //int mid = (low + high) >> 1;
+            /*
+            Line 11: Char 28: runtime error: signed integer overflow: 1063376696 + 2126753390 cannot be represented in type 'int' (solution.cpp)
+SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior prog_joined.cpp:22:28
+
+Last executed input
+2126753390
+1702766719
+            */
             int mid = low + (high - low)/2;
             
-            if(isBadVersion(mid)) {
+            if(isBadVersion(mid)){
                 ans = mid;
-                high = mid - 1;
+                high = mid -1;
             }
-            else{
+            
+            else if(!isBadVersion(mid)){
                 low = mid + 1;
             }
         }
