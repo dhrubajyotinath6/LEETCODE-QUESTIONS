5397fd0e58f86862ade279ee313b37212383b86b
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-02-28 22:25:41 +0530

    Time: 24 ms (86.10%), Space: 25 MB (96.13%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -9,6 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+class Solution {
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<string, int> cnt;
+        vector<TreeNode*> res;
+        function<string(TreeNode*)> traverse = [&cnt, &res, &traverse](TreeNode* node) -> string {
+            if (node == nullptr) {
+                return "";
+            }
+            string representation = "(" + traverse(node->left) + ")" + to_string(node->val) + "(" +
+                                    traverse(node->right) + ")";
+            cnt[representation]++;
+            if (cnt[representation] == 2) {
+                res.push_back(node);
+            }
+            return representation;
+        };
+        traverse(root);
+        return res;
+    }
+};
+
+tc, sc -> O(N^2)
+
+*/
+
+
+//optimal approach
 class Solution {
     
 public:
@@ -46,3 +76,5 @@ public:
         return res;
     }
 };
+
+//tc, sc -> O(N)
