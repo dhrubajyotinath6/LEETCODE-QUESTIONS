692f1e05a7722dc2fe6928e3dd2bbdd89581bc52
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-05-13 14:41:52 +0530

    Time: 274 ms (32.66%), Space: 14.3 MB (77.32%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,69 +1,156 @@
+//Approach 1: Depth-First Search (DFS)
 /*
-4 approach
-
-dfs
-union
-optimized dfs
-optimized union
-
-*/
-
-//dfs
 class Solution {
-    
 public:
     // Return true if stone a and b shares row or column
     int shareSameRowOrColumn(vector<int>& a, vector<int>& b) {
         return a[0] == b[0] || a[1] == b[1];
     }
     
-public:
-    void dfs(vector<vector<int>> &graph, vector<int> &visit, int src){
+    void dfs(vector<vector<int>>& stones, vector<int> adj[], vector<int>& visited, int src) {
+        // Mark the stone as visited
+        visited[src] = 1;
         
-        visit[src] = 1;
-        
-        for(auto &child : graph[src]){
-            // cout << src << " " << child;
-            if(visit[child] == 0){
-                dfs(graph, visit, child);
+        // Iterate over the adjacent, and iterate over it if not visited yet
+        for (int adjacent : adj[src]) {
+            if (visited[adjacent] == 0) {
+                dfs(stones, adj, visited, adjacent);
             }
         }
     }
     
-public:
-    int countComponents(int n, vector<vector<int>>& stones) {
-        
-        if(n == 0) return 0;
-        int comp = 0;
-        
-        vector<int> visit(n, 0);
-        vector<vector<int>> graph(n);
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < stones.size(); j++){
-                if(shareSameRowOrColumn(stones[i], stones[j])){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
+    int removeStones(vector<vector<int>>& stones) {
+        // Adjacency list to store edges
+        vector<int> adj[stones.size()];
+        for (int i = 0; i < stones.size(); i++) {
+            for (int j = i + 1; j < stones.size(); j++) {
+                if (shareSameRowOrColumn(stones[i], stones[j])) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
         
-        for(int i = 0; i < n; i++){
-            
-            if(!visit[i]){
-                comp++;
-                dfs(graph, visit, i);
+        // Array to mark visited stones
+        vector<int> visited(stones.size(), 0);
+        // Counter for connected components
+        int componentCount = 0;
+        for (int i = 0; i < stones.size(); i++) {
+            if (visited[i] == 0) {
+                // If the stone is not visited yet,
+                // Start the DFS and increment the counter
+                componentCount++;
+                dfs(stones, adj, visited, i);
             }
         }
-        return comp;
-    }
-    
-public:
-    int removeStones(vector<vector<int>>& stones) {
         
-        int comp = countComponents(stones.size(), stones);
-        cout << comp << endl;
-        cout << stones.size() << endl;
-        return stones.size() - comp;
+        // Return the maximum stone that can be removed
+        return stones.size() - componentCount;
     }
 };
+
+*/
+
+//Approach 2: Disjoint Set Union (DSU)
+
+class Solution {
+public:
+    // Return true if stone a and b shares row or column
+    int shareSameRowOrColumn(vector<int>& a, vector<int>& b) {
+        return a[0] == b[0] || a[1] == b[1];
+    }
+    
+    // Returns the representative of vertex x
+    int find(vector<int>& rep, int x) {
+        if (x == rep[x]) {
+            return x;
+        }
+        // Uses Path compression
+        return rep[x] = find(rep, rep[x]);
+    }
+    
+    // Combine the stone x and y, and returns 1 if they were not connected
+    int performUnion(vector<int>& rep, vector<int>& size, int x, int y) {
+        x = find(rep, x);
+        y = find(rep, y);
+        
+        if (x == y) {
+            return 0;
+        }
+        
+        if (size[x] > size[y]) {
+            rep[y] = x;
+            size[x] += size[y];
+        } else {
+            rep[x] = y;
+            size[y] += size[x];
+        }
+        
+        return 1;
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        vector<int> rep(stones.size());
+        vector<int> size(stones.size());
+        // Initialize rep to itself and size as 1
+        for (int i = 0; i < stones.size(); i++) {
+            rep[i] = i;
+            size[i] = 1;
+        }
+        
+        int componentCount = stones.size();
+        for (int i = 0; i < stones.size(); i++) {
+            for (int j = i + 1; j < stones.size(); j++) {
+                if (shareSameRowOrColumn(stones[i], stones[j])) {
+                    // Decrement the componenets if union invloved merging
+                    componentCount -= performUnion(rep, size, i, j);
+                }
+            }
+        }
+        
+        // Return the maximum stone that can be removed
+        return stones.size() - componentCount;
+    }
+};
+
+
+
+/*
+4 approach
+
+dfs
+union
+optimized dfs  
+optimized union
+
+https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/solution/
+
+*/
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
