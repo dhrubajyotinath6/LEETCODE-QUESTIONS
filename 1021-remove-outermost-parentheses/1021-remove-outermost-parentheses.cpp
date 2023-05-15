c32fa5fae5dff054c4ecdbe6e845d5ee5212c88d
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-05-15 11:54:56 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (80.07%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,16 +1,30 @@
-//https://leetcode.com/problems/remove-outermost-parentheses/discuss/270022/JavaC%2B%2BPython-Count-Opened-Parenthesis
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-       
-        int opened = 0;
-        string res;
         
-        for(auto &c : s){
-            if(c == '(' && opened++ > 0) res += c;
-            if(c == ')' && --opened > 0) res += c;
-        }
+        int bal = 0;
+        string ans = "";
         
-        return res;
+        for(auto& ch : s){
+            
+            if(ch == '('){
+                
+                if(bal != 0){
+                    ans += ch;
+                }
+                
+            }else{ // ch == ')'
+                
+                if(bal > 1){
+                    ans += ch;
+                }
+            }
+            
+            bal += (ch == '(') ? 1:-1;
+            
+            }
+        
+        
+        return ans;
     }
 };
