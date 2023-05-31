410d7600c649a146e5ee5b0e923054a1555c314f
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-05-31 20:52:53 +0530

    Update 0976-largest-perimeter-triangle.cpp

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -12,6 +12,27 @@ a < b < c
 so if a + b > c satifies then it will be a triange
 bcoz of sorting other two conditions will always remain true;
 
+
+Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, 
+formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,2]
+Output: 5
+Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+Example 2:
+
+Input: nums = [1,2,1,10]
+Output: 0
+Explanation: 
+You cannot use the side lengths 1, 1, and 2 to form a triangle.
+You cannot use the side lengths 1, 1, and 10 to form a triangle.
+You cannot use the side lengths 1, 2, and 10 to form a triangle.
+As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+
 */
 
 class Solution {
