7e5f24bc6d03afaba475cb940b67f53cac21e77c
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-06-01 11:33:18 +0530

    Update 1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -34,7 +34,8 @@ Whenever we consider any number time[i] we can have 2 possibilities :
 
 We basically need to consider the time[i]%60 each time to know if it is==0 or not.
 1) If modulo_val==0 we simply do count+=hash[modulo_val]
-2) Else we need to find out how far is time[i] away from its next 60 multiple i.e 100 is 20 far from 120 which is a multiple of 60 so if we have a 20 alreay existing then (100,20) can make a valid pair.
+2) Else we need to find out how far is time[i] away from its next 60 multiple i.e 100 is 20 far from 120 which is a multiple of 60 
+so if we have a 20 alreay existing then (100,20) can make a valid pair.
 3) To achieve this we first do time[i]%60 then subtract it from 60. Like 100 % 60=40 and 60-40=20. So count+=hash[20]
 
 */
