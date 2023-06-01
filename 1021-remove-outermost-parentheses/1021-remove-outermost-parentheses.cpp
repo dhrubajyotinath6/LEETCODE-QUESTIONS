217961d40b6e351d62985459beef086671921577
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-06-01 11:48:52 +0530

    Update 1021-remove-outermost-parentheses.cpp

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,3 +1,46 @@
+
+/*class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        stack<int> st;
+        string str = "";
+        for(int i = 0; i < s.size(); i++){
+            
+            char ch = s[i];
+            
+            if(ch == '('){
+                
+                if(st.size() > 0)   str+=ch;
+                st.push(ch);
+            }
+            else{
+                st.pop();
+                if(st.size() > 0)   str+=ch;
+            }
+        }
+        return str;
+    }
+};
+*/
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        string res;
+        int open = 0;
+        
+        for(char c : s){
+            if(c == '(' && open++ > 0) res += c;
+            if(c == ')' && open-- > 1) res += c;
+        }
+        return res;
+    }
+};
+
+
+
+
 class Solution {
 public:
     string removeOuterParentheses(string s) {
