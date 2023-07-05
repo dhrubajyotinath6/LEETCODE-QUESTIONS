02d8fd109420bb4ae0cdc53ef2d3ef7dcf25cf68
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-07-05 07:08:23 +0530

    Update 338-counting-bits.cpp

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -25,3 +25,21 @@ public:
         return ans;
     }
 };
+
+
+//BEST SOLUTION  -> from techDose
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> res(n+1);
+        res[0] = 0;
+        
+        for(int i = 1; i <= n; i++){
+            res[i] = res[i/2] + i%2;
+        }
+        
+        return res;
+    }
+};
