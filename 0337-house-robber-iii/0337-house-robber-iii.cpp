48be684032238e4cda773c427f067074c38ca4a3
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-07-17 20:51:39 +0530

    Time: 28 ms (30.39%), Space: 31.7 MB (8.94%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  
+    // 0 -> not rob,   1 -> rob
+public:
+    vector<int> robSub(TreeNode* root){
+        
+        if(root == NULL) return {0, 0};
+        
+        vector<int> left = robSub(root->left);
+        vector<int> right = robSub(root->right);
+        
+        vector<int> res(2);
+        res[0] = max(left[0], left[1]) + max(right[0], right[1]);
+        res[1] = root->val + left[0] + right[0];
+        
+        return res;
+     } 
+public:
+    int rob(TreeNode* root) {
+        vector<int>res = robSub(root);
+        return max(res[0], res[1]);
+    }
+};
+
+
+
+
+
