c3afc724836dbce61adb89b660da2869c96a47ec
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-07-18 14:27:11 +0530

    Time: 139 ms (5.00%), Space: 46.6 MB (5.37%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -4,6 +4,7 @@
 
 //https://leetcode.com/problems/all-paths-from-source-to-target/discuss/986429/Python-Iterative-DFS-with-detailed-time-complexity-and-visuals
 
+/*
 class Solution {
     
 public:
@@ -27,4 +28,31 @@ public:
         return paths;
     }
 };
+*/
+
+class Solution {
+    
+public:
+    void dfs(vector<vector<int>> graph, vector<vector<int>> &paths, vector<int> &path, int curr){
+        path.push_back(curr);
+        
+        if(curr == graph.size() - 1) paths.push_back(path);
+        else{
+            
+            for(auto& it : graph[curr])
+                dfs(graph, paths, path, it);
+        }
+        path.pop_back();
+    }
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> paths;
+        vector<int> path;
+        
+        dfs(graph, paths, path, 0);
+        return paths;
+    }
+};
+
+
 
