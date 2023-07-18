2c6f9f76392f1f98670b7f2e8f1a6ad3e8797d61
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-07-18 14:26:28 +0530

    Time: 135 ms (5.53%), Space: 46.6 MB (5.37%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,30 @@
+//acyclic graph  -- no cycle -- very very imp
+
+//https://leetcode.com/problems/all-paths-from-source-to-target/discuss/118691/C%2B%2BPython-Backtracking
+
+//https://leetcode.com/problems/all-paths-from-source-to-target/discuss/986429/Python-Iterative-DFS-with-detailed-time-complexity-and-visuals
+
+class Solution {
+    
+public:
+    void dfs(vector<vector<int>> graph, vector<vector<int>> &paths, vector<int> &path, int curr){
+        path.push_back(curr);
+        
+        if(curr == graph.size() - 1) paths.push_back(path);
+        else{
+            
+            for(auto& it : graph[curr])
+                dfs(graph, paths, path, it);
+        }
+        path.pop_back();
+    }
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> paths;
+        vector<int> path;
+        
+        dfs(graph, paths, path, 0);
+        return paths;
+    }
+};
+
