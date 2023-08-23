82e181e3f26a6f40d34947cd37f487542828fe30
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-08-23 10:02:21 +0530

    Update 766-toeplitz-matrix.cpp

--- 766-toeplitz-matrix/766-toeplitz-matrix.cpp
+++ 766-toeplitz-matrix/766-toeplitz-matrix.cpp
@@ -1,4 +1,28 @@
 //https://leetcode.com/problems/toeplitz-matrix/solution/
+
+Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+
+Input: matrix = [[1,2],[2,2]]
+Output: false
+Explanation:
+The diagonal "[1, 2]" has different elements.
+
 /*
 Approach #1: Group by Category [Accepted]
 Intuition and Algorithm
