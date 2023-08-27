9c4c673d0c8a9bb3871ba3321cd83278ae740363
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-08-27 20:30:16 +0530

    Update 0653-two-sum-iv-input-is-a-bst.cpp

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,6 +11,23 @@
  
  https://leetcode.com/problems/two-sum-iv-input-is-a-bst/solution/
  check approch 3 -> inorder transversal, then apply 2 sum
+
+ Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their 
+ sum is equal to k, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+
  */
 class Solution {
 
