ba76b836896eb5369e2eebdef95c76255de512b3
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-09-25 11:23:21 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,22 +1,20 @@
-//https://leetcode.com/problems/find-the-difference/solution/
-
 class Solution {
 public:
     char findTheDifference(string s, string t) {
         
-        // Initialize ch with 0, because 0 ^ X = X
-        // 0 when XORed with any bit would not change the bits value.
-        char ch = 0;
-
-        // XOR all the characters of both s and t.
-        for (int i = 0; i < s.length(); i += 1) {
-            ch ^= s[i];
+        vector<int> freq(26,0);
+        
+        for(auto& ch : s){
+            freq[ch - 'a']++;
         }
-        for (int i = 0; i < t.length(); i += 1) {
-            ch ^= t[i];
+        
+        for(auto& ch : t){
+            freq[ch - 'a']--;
+            
+            if(freq[ch - 'a'] == -1) return ch;
         }
-
-        // What is left after XORing everything is the difference.
-        return ch;
+        
+        
+        return 'a';
     }
 };
