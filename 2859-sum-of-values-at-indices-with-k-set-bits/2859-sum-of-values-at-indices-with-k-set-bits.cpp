c66476826e79c1f6ce0d91a66fa161003dbaf1c2
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-10-31 15:50:50 +0530

    Time: 15 ms (14.55%), Space: 21.9 MB (45.50%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/sum-of-values-at-indices-with-k-set-bits/discuss/4063345/Hacker's-Delight
 
+/*
 class Solution {
     
 public:    
@@ -28,4 +29,41 @@ public:
         
     }
 };
+
+*/
+
+//The getNextSamePopcount function (taken from the Hacker's Delight book) returns the next bigger number with the same number of bits set.
+
+
+
+
+
+
+class Solution {
+    
+public:    
+ int getNextSamePopcount(int a) {
+    if (a == 0)
+        return INT_MAX;
+  int c = (a & -a), r = a + c;
+  return ((((r ^ a) >> 2) / c) | r);
+}
+    
+public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int res = 0;
+        for (int i = (1 << k) - 1; i < nums.size(); i = getNextSamePopcount(i))
+            res += nums[i];
+        return res;
+    }
+        
+    
+};
+
+/*
+k = 1,    start = 1 (1)
+k = 2,    start = 3 (11)
+k = 3,    start = 7 (111) -> 7 = 8 - 1 => (1 << k) - 1
+
+*/
 
