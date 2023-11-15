400a25660eb44f2659f98b8ec72eeebc1fb296a7
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-11-15 23:10:08 +0530

    Time: 68 ms (94.40%), Space: 53.8 MB (5.22%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -18,3 +18,24 @@ public:
         return ans;
     }
 };
+
+
+/*
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int ans = 1;
+        
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] >= ans + 1) {
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+*/
