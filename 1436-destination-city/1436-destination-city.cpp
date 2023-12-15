aada2f9b82204689410efaf140ba1bc14a3fe732
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-12-15 15:13:27 +0530

    Time: 11 ms (50.48%), Space: 11.2 MB (46.17%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -44,3 +44,29 @@ public:
         return ans;
     }
 };
+
+
+//better official solution
+
+/*
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> hasOutgoing;
+        for (int i = 0; i < paths.size(); i++) {
+            hasOutgoing.insert(paths[i][0]);
+        }
+        
+        for (int i = 0; i < paths.size(); i++) {
+            string candidate = paths[i][1];
+            if (hasOutgoing.find(candidate) == hasOutgoing.end()) {
+                return candidate;
+            }
+        }
+        
+        return "";
+    }
+};
+
+
+*/
