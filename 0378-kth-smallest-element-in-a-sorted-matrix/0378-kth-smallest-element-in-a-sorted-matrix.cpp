7930d6ea43975e25bb3d7a747f3bcca0230c2529
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-12-20 00:14:53 +0530

    Time: 25 ms (67.81%), Space: 13.4 MB (92.84%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
@@ -18,3 +19,60 @@ public:
         return pq.top();
     }
 };
+*/
+
+//https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/discuss/1322101/C%2B%2BJavaPython-MaxHeap-MinHeap-Binary-Search-Picture-Explain-Clean-and-Concise
+
+
+class Solution {
+    
+public:
+    int countsLessThanMid(vector<vector<int>>& matrix, int mid){
+        
+        int count = 0;
+        int m = matrix.size(), n = matrix[0].size(); 
+        
+        int c = n-1;
+        
+        for(int r = 0; r < m; r++){
+            while(c >= 0 && matrix[r][c] > mid) c--;
+            
+            count += (c+1);
+        }
+        
+        return count;
+    }
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        int m = matrix.size(), n = matrix[0].size();
+        
+        int low = matrix[0][0],  high = matrix[m-1][n-1];
+        int ans = -1;
+        
+        while(low <= high){
+            
+            int mid = (low + high) >> 1;
+            
+            if(countsLessThanMid(matrix, mid)  >= k){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
