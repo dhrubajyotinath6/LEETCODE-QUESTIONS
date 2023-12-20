7138de55a531333b6760d2bfe746c23383deeebe
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-12-20 11:43:08 +0530

    Time: 4 ms (70.57%), Space: 6.8 MB (83.07%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -25,6 +25,8 @@ public:
         
         for(int currUnique = 1; currUnique <= maxUnique; currUnique++){
             
+            //////////////////
+            
             memset(countMap, 0, sizeof(countMap));
             int windowStart = 0, unique = 0;
             int i = 0;
@@ -54,8 +56,65 @@ public:
                     result = max(result, windowEnd - windowStart);
                 }
             }
+            
+            ///////////////////
         }
         
         return result;
     }
 };
+
+
+/*
+s = "a b a b a b b d a b c a b c",  k = 2;
+
+maxUnique = 3
+
+currUnique = 1
+s = a b a b a [b b] d a b c a b c  -> 2
+
+currUnique = 2
+s = [a b a b a b b] d a b c a b c  -> 7
+
+currUnique = 3
+s = a b a b a b b d [a b c a b c]  -> 6
+
+
+res = max(2,7,6) = 7
+
+
+
+Algorithm
+
+Find the number of unique characters in the string s and store the count in variable maxUnique. For s = aabcbacad, the unique characters are a,b,c,d and maxUnique = 4.
+
+Iterate over the string s with the value of currUnique ranging from 1 to maxUnique. In each iteration, currUnique is the maximum number of unique characters that must be present in the sliding window.
+
+The sliding window starts at index windowStart and ends at index windowEnd and slides over string s until windowEnd reaches the end of string s. At any given point, we shrink or expand the window to ensure that the number of unique characters is not greater than currUnique.
+
+If the number of unique character in the sliding window is less than or equal to currUnique, expand the window from the right by adding a character to the end of the window given by windowEnd
+
+Otherwise, shrink the window from the left by removing a character from the start of the window given by windowStart.
+
+Keep track of the number of unique characters in the current sliding window having at least k frequency given by countAtLeastK. Update the result if all the characters in the window have at least k frequency.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
