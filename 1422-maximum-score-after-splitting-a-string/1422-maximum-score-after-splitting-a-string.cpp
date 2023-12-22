3dd19fa8d6c552664ffd9ee5bc7a567e6f710c0c
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-12-22 12:09:43 +0530

    Time: 3 ms (50.89%), Space: 6.7 MB (62.95%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,3 +1,7 @@
+/*
+tc - O(n)
+sc - O(n)
+
 class Solution {
 public:
     int maxScore(string s) {
@@ -5,11 +9,11 @@ public:
         int ans = 0;
         int n = s.size();
         
-        /*
-        0   1   1   1   0   1
-        4   4   3   2   1   1 --suffix
         
-        */
+       // 0   1   1   1   0   1
+       //4   4   3   2   1   1 --suffix
+        
+        
         vector<int> suffix(n);
         suffix[n-1] = s[n-1] - '0';
         
@@ -37,5 +41,64 @@ public:
         }
         
         return ans;
+    }
+};
+*/
+
+/*
+tc - O(n)  -- 2 pass
+sc - O(1)
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int ones = count(s.begin(), s.end(), '1');
+        
+        int ans = 0;
+        int zeros = 0;
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (s[i] == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
+            
+            ans = max(ans, zeros + ones);
+        }
+        
+        return ans;
+    }
+};
+*/
+
+/*
+score = Zeros(left) + Ones(right);
+
+Ones(right) = Ones(Total) - Ones(Left);
+
+score = Zeros(left) + Ones(Total) - Ones(Left);
+      = Zeros(left) - Ones(Left) + Ones(Total);
+      = best + Ones(Total)
+      
+Here Ones(Total) is constant
+
+*/
+
+class Solution {
+public:
+    int maxScore(string s) {
+        
+        int ones = 0, zeros = 0, best = INT_MIN;// best = 0 will not work see s = "1111"
+        
+        for(int i = 0; i <= s.size() - 2; i++){
+            if(s[i] == '1') ones++;
+            else zeros++;
+            
+            best = max(best, zeros - ones);
+        }
+        
+        if(s[s.size() - 1] == '1') ones++;
+        
+        return best + ones;
     }
 };
