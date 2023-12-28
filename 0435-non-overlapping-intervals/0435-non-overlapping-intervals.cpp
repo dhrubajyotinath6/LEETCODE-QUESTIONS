f505b3242ba28a8efc46255c0503edf4c13231a9
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-12-28 19:01:25 +0530

    Time: 2335 ms (5.64%), Space: 506.2 MB (8.46%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,8 +1,45 @@
+//https://leetcode.com/problems/non-overlapping-intervals/discuss/792726/C%2B%2B-Simple-O(NlogN)-solution-with-explanation
+
+//https://leetcode.com/problems/non-overlapping-intervals/discuss/1157602/Easy-C%2B%2B-solution-with-drawing
+
+/*
+we have to sort based on end point:
+
+why not start point??
+ANS : suppose you have cases like : (1,8), (2,3), (3,4), (5,9)
+if you sort in the basis of start points you will end up considering (1,8) and deleting rest which collide with (1,8).
+For a greedy approach you will want the point with lower end point to be considered.
+
+
+             -------------
+Case 1:-
+------------------   -----------------
+|   Interval 1   |   |   Interval 2  |
+------------------   -----------------
+
+Case 2:-
+------------------
+|   Interval 1   |
+------------------
+           ------------------
+           |    Interval 2  |
+           ------------------
+
+Case 3:-
+ --------------------
+ |    Interval 1    |
+ --------------------
+    --------------
+    | Interval 2 |
+    --------------
+
+*/
+
 class Solution {
     
 public:
     static bool comp(vector<int> a, vector<int> b){
-        return b[1] > a[1];
+        return b[1] > a[1]; // sort based on end points
     }
         
 public:
