8de0fa3e0e190aa76bd4510975ec5d1d2832a1c8
DHRUBA JYOTI NATH <95700412+dhrubajyotinath6@users.noreply.github.com>
2023-12-29 20:08:06 +0530

    Time: 241 ms (52.57%), Space: 10.6 MB (63.62%) - LeetHub

---
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -0,0 +1,46 @@
+
+/*
+tc - O(nk) ; k = primes.length
+sc - O(n)
+*/
+
+#define ll long long int
+
+class Solution {
+    
+public:
+    ll minFunc(ll a, ll b){
+        if(a < b) return a;
+        return b;
+    }
+    
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        
+        vector<ll> pointers(primes.size(), 0);
+        
+        vector<ll> dp(n, 0);
+        dp[0] = 1;
+        
+        for(int i = 1; i < n; i++){
+            
+            int mini = INT_MAX;
+            
+            for(int i = 0; i < pointers.size(); i++){
+                mini =  minFunc(mini, dp[pointers[i]] * primes[i]);
+            }
+            
+            for(int i = 0; i < pointers.size(); i++){
+                if(mini == dp[pointers[i]]*primes[i]){
+                   pointers[i]++; 
+                }
+            }
+            
+            dp[i] = mini;
+        }
+        
+        int ans = dp[n-1];
+        return ans;
+        
+    }
+};
